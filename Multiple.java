e525871c83612f41fed7164ca0b98b2d04ed240a
Ardnahcivar01 <134200599+Ardnahcivar01@users.noreply.github.com>
2023-05-23 15:25:10 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,28 @@
+package Inheritence;
+
+class Animal{
+private
+    int age = 12;
+    String bread = "German";
+public 
+    void show_age(){
+        System.out.println(age);
+    }
+    void bread(){
+        System.out.println(bread);
+    }
+}
+class cat {
+private
+    int Quan = 10;
+public 
+    void cat_Quantity(){
+        System.out.println(Quan);
+    }
+}
+
+ class Multiple {
+    public static void main(String s[]){
+
+    }
+}
