4d6b512806bda5ec9edbd78f8102f471b9a8ce1e
Ardnahcivar01 <134200599+Ardnahcivar01@users.noreply.github.com>
2023-05-23 15:29:43 +0530

    Add files via upload

---
+++ linear.java
@@ -0,0 +1,16 @@
+public class linear {
+    public static int linear(int[] arr,int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return 0;
+
+    }
+    public static void main(String s[]){
+        int[] arr = {1,2,3,4,5};
+        int key =Integer.parseInt(s[0]);
+        System.out.println(key+" Found in arrray at index : "+linear(arr,key));
+    }
+}
