941113ea5f6e82fb00684839c0f3bd47a9a38598
Ardnahcivar01 <134200599+Ardnahcivar01@users.noreply.github.com>
2023-05-23 15:46:13 +0530

    Add files via upload

---
+++ quad.java
@@ -0,0 +1,21 @@
+public class quad {
+    public static void main(String s[]){
+       
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        int c = Integer.parseInt(s[2]);
+
+        if(((b*b)-4*a*c)<0){
+            System.out.println("Solution of Quadtric equation is negative ((Then not possible )) !! ");
+        }
+        else if(((b*b)-4*a*c)>0)
+        {
+            int eq = ((b*b)-4*a*c);
+            System.out.println("Positive then solution is : "+eq);
+        }
+        else{
+            System.out.print("solutioin of Quadtric equation is Zero(0) !!");
+        }
+    }
+    
+}
