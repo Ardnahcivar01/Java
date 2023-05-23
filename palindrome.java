6ed74aa6f10e45fd3ba20cae8024859463e922e2
Ardnahcivar01 <134200599+Ardnahcivar01@users.noreply.github.com>
2023-05-23 15:28:08 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String a[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number :");
+        int num = sc.nextInt();
+        int sum =0,r,temp;
+        temp = num;
+        while(num>0){
+            r = num%10;
+            sum =(sum*10)+r;
+            num = num/10;
+        }
+        if(temp==sum){
+            System.out.println("Number is palindrome");
+        }
+        else{
+            System.out.println("NUmber is not palindrome");
+        }
+    }
+}
